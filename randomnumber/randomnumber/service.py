c605c40a820edbfdbb3ea77d819a484b091489bf
Alistair Bleeck <ableeck@users.noreply.github.com>
2022-05-18 15:49:35 -0400

    Add origin server header (#1)
    * Allow settting the secret key as environment variable
    
    * Set X-Origin-Server header
    
    * black / isort

--- randomnumber/randomnumber/service.py
+++ randomnumber/randomnumber/service.py
@@ -1,5 +1,11 @@
+from socket import gethostname
+
 from .models import RandomNumber
 
 
 def get_random_number() -> RandomNumber:
     return RandomNumber.objects.create()
+
+
+def get_host() -> str:
+    return str(gethostname())
