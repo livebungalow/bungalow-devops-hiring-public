166785defab0cccbfeb468a124ae6f68a757d72e
Mustafa EL-Hilo <melhilo@bungalow.com>
2022-05-18 16:25:45 -0700

    removed command

--- terraform/ec2_worker_1.tf
+++ terraform/ec2_worker_1.tf
@@ -85,7 +85,6 @@ resource "aws_instance" "worker_1_exec" {
       "source .venv/bin/activate",
       "python -m pip install -r requirements.txt",
       "python manage.py migrate",
-      "python manage.py createsuperuser",
       "python manage.py runserver",
     ]
   }
