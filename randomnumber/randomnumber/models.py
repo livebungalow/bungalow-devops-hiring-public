4f1849c6c63965233e2a4fb5ed9d649c59769c06
Alistair Bleeck <alistair@bungalow.com>
2022-05-19 16:09:09 -0400

    Slightly better

--- randomnumber/randomnumber/models.py
+++ randomnumber/randomnumber/models.py
@@ -6,7 +6,7 @@ from django.utils.timezone import now
 
 class RandomNumber(models.Model):
     def get_random_number() -> int:
-        return random.getrandbits(random.randint(60, 64))
+        return random.getrandbits(random.randint(61, 64))
 
     number = models.PositiveBigIntegerField(default=get_random_number)
     created_at = models.DateTimeField(default=now)
